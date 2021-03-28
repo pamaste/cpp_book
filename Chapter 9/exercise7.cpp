6cff643d759b550af1adf9844fd783db99594f18
pamaste <33986334+pamaste@users.noreply.github.com>
2021-03-28 18:18:10 -0400

    Update exercise7.cpp
    Added two decimal places for percentage output.
    Corrected winning candidate output.
    Adjusted printResults to correctly verify input in Cengage.

--- Chapter 9/exercise7.cpp
+++ Chapter 9/exercise7.cpp
@@ -13,6 +13,7 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -20,50 +21,54 @@ int findWinner(int votes[]);
 void printResults(string candidates[], int votes[]);
 double calculatePercentage(int votes[], int vote);
 
-const int NUMBER_OF_CANDIDATES = 5;
+const int numberOfCandidates = 5;
 
 int main()
 {
     
-    string candidates[NUMBER_OF_CANDIDATES];
-    int votes[NUMBER_OF_CANDIDATES];
+    string candidates[numberOfCandidates];
+    int votes[numberOfCandidates];
+
     
-    cout << "Please input the 5 candidates followed by the votes they recieved i.e. Smith 5000: ";
-    for (int i = 0; i < NUMBER_OF_CANDIDATES; i++) {
+    cout << "Enter 5 candidates followed by number of votes recieved: ";
+    for (int i = 0; i < numberOfCandidates; i++) {
         cin >> candidates[i] >> votes[i];
     }
     printResults(candidates, votes);
-    cout << "And the winner is: " << candidates[findWinner(votes)] << endl;
+    cout << "The Winner of the Election is " << candidates[findWinner(votes)] << endl;
     return 0;
 }
 
 double calculatePercentage(int votes[], int vote){
     int sumOfVotes = 0;
     
-    for (int i = 0; i < NUMBER_OF_CANDIDATES; i++) {
+    for (int i = 0; i < numberOfCandidates; i++) {
         sumOfVotes += votes[i];
     }
     
-    double percentage = static_cast<double>(vote) / sumOfVotes;
-    
+    double percentage = static_cast<double>(vote) / static_cast<double>(sumOfVotes);
+
     return percentage*100;
 }
 
 
 void printResults(string candidates[], int votes[]){
-    cout << "Name:" << setw(15) << "Votes:" << setw(15) << "Percentage:" << endl;
-    for (int i = 0; i < NUMBER_OF_CANDIDATES; i++) {
+    cout << "Candidate Votes Received % of Total Votes" << endl;
+    cout << fixed << showpoint << setprecision(2) << endl;
+    for (int i = 0; i < numberOfCandidates; i++) {
+        
         cout << candidates[i] << setw(15) << votes[i] << setw(15);
-        int percentage = calculatePercentage(votes, votes[i]);
-        cout << percentage << "%" << endl;
+        double percentage = calculatePercentage(votes, votes[i]);
+        
+        cout << percentage << endl;
         
     }
 }
 
 int findWinner(int votes[]){
     int winner = 0;
-    for (int i = 0; i < NUMBER_OF_CANDIDATES; i++) {
-        if (votes[i] > winner)
+    for (int i = 0; i < numberOfCandidates; i++) {
+        if (votes[winner] <= votes[i])
             winner = i;
     }
     return winner;
